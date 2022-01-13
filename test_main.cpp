c40eed37f586b904d1484fd3a440a86bc084cf37
Felix Sonnleitner <felix.sonnleitner@gmx.at>
2022-01-13 10:20:05 +0100

    Update test_main.cpp

--- test_main.cpp
+++ test_main.cpp
@@ -2,19 +2,38 @@
 // Created by Felix on 13.01.2022.
 //
 
-#include <he
+#include "helperfunctions.h"
 
 bool test01()
 {
-    auto fileName = "doesNotExist.csv"
-    
+    char* fileName = "C:\\Users\\Felix\\CLionProjects\\interview\\test.csv";
+    auto numbers = helperFunctions::readNumbers(fileName, ' ');
     return true;
+}
+
+bool test02()
+{
+    char* fileName = "C:\\Users\\Felix\\CLionProjects\\interview\\test.csv";
+    auto numbers = helperFunctions::readNumbers(fileName, ' ');
+    auto outOfBounds = helperFunctions::outOfBounds(numbers);
+    return outOfBounds == 0;
+}
+
+
+bool test03()
+{
+    char* fileName = "C:\\Users\\Felix\\CLionProjects\\interview\\test1.csv";
+    auto numbers = helperFunctions::readNumbers(fileName, ' ');
+    auto outOfBounds = helperFunctions::outOfBounds(numbers);
+    return outOfBounds != 0;
 }
 
 int main()
 {
     bool ok = true;
     ok &= test01();
+    ok &= test02();
+    ok &= test03();
     
     
     return ok ? 0 : 1;
